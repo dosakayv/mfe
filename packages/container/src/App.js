c13e04fc9711fca02e8a587cd8c834324f53a0ce
Vishnu Dosakayala <dosaka@amazon.com>
2024-01-29 13:32:05 -0800

    Updated application header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return <div>
-        <h1>Hi there! It's Me - Test</h1>
+        <h1>Hi there! It's Me</h1>
         <hr />
         <MarketingApp />
     </div>;
